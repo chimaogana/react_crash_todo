a2b056e041646db2b71df64b32f46c501b3f8e04
chimaogana <clevadani@gmail.com>
2020-08-18 10:10:12 +0100

    Updates

---
+++ precache-manifest.772ad29e5ef806deb846b962d7682ece.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55aa9754445441e689a5653743f9da71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5920d36b989720a2ff92",
+    "url": "/static/css/main.23ba70ae.chunk.css"
+  },
+  {
+    "revision": "919a334d1d6d083f1c7c",
+    "url": "/static/js/2.dec122a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dec122a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5920d36b989720a2ff92",
+    "url": "/static/js/main.5eb515bc.chunk.js"
+  },
+  {
+    "revision": "26067943b6744300770a",
+    "url": "/static/js/runtime-main.8319d428.js"
+  }
+]);
