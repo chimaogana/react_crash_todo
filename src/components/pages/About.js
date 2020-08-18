ad719ca6f499ffedf657a323da680a5a4dc602a9
chimaogana <clevadani@gmail.com>
2020-08-18 09:33:49 +0100

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+	return (
+		<React.Fragment>
+			<h1>
+				<p>
+					This is the TodoList app v1.0.0. It is part of a React crash course
+				</p>
+			</h1>
+		</React.Fragment>
+	);
+}
+
+export default About;
